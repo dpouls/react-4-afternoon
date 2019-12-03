a7995e0292d9e8191d34246f8169a20f3f98f0cf
dpouls <dallinpoulson@gmail.com>
2019-12-03 15:51:13 -0700

    finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link,Switch,Route } from "react-router-dom";
+import History from '../History/History'
+import Contact from '../Contact/Contact'
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link className="subnav_links" to="/about">
+            <h3>About</h3>
+          </Link>
+          <Link to="/about/history" className="subnav_links">
+            <h3>History</h3>
+          </Link>
+          <Link to="/about/contact" className="subnav_links">
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
+        <div className="box">
+          <Switch>
+            <Route path='/about/history' component={History}/>
+            <Route path='/about/contact' component={Contact}/>
+            <Route path='/about' 
+              render= {() => (
+                <div>
+                  <h1>About the University</h1>
+                  <p>
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    Curse you Perry the Platypus.
+                    </p>
+                </div>
+              )}
+            />
+          </Switch>
         </div>
       </div>
-    )
+    );
   }
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import {Link} from 'react-router-dom'
 
 export default class Student extends Component {
   constructor() {
     super()
-
+    this.state = {
+      studentInfo: {}
+    }
   }
-
+  componentDidMount(){
+    Axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(results => {
+      console.log(this.props.match)
+      this.setState({
+        studentInfo: results.data
+      })
+    })
+  }
   render() {
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student:</h1>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <Link to={`/classlist/${this.state.studentInfo.class}`}><button>Back</button></Link>
       </div>
     )
   }
